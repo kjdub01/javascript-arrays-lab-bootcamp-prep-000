f62ccbdcf8715731a65c01012c1d47bdeb24dc79
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-31 01:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,34 +5,4 @@ function destructivelyAppendKitten(name, "Ralph") {
   return Array
 }
 
-function destructivelyPrependKitten(name, "Bob") {
-  Array.unshift("Bob")
-  return Array
-}
-
-function desructivelyRemoveLastKitten(name) {
-  Array.pop()
-  return Array
-}
-
-function destructivelyRemoveFirstKitten(name) {
-  Array.shift()
-  return Array
-}
-
-function appendKitten() {
-  
-}
-
-function prependKitten() {
-  
-}
-
-function removeLastKitten() {
-  
-}
-
-function removeFirstKitten () {
-  
-}
 
