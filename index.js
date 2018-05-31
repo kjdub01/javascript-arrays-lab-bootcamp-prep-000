279160646a5d9b772ba44318c51487d31b8c0bdc
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-31 01:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-kitten = new Array ("Milo", "Otis", "Garfield")
+var kitten = new Array ("Milo", "Otis", "Garfield")
 function destructivelyAppendKitten(name) {
   kitten.push("Ralph")
   return array
