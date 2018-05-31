e37098c78ae27a00d186da1ef948de5d6ce3b817
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-31 01:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name, "Ralph") {
+  Array.push("Ralph")
+  return Array
+}
+
+function destructivelyPrependKitten(name, "Bob") {
+  Array.unshift("Bob")
+  return Array
+}
+
+function desructivelyRemoveLastKitten(name) {
+  Array.pop()
+  return Array
+}
+
+function destructivelyRemoveFirstKitten() {
+  Array.shift()
+  return Array
+}
+
+function appendKitten() {
+  
+}
+
+function prependKitten() {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten () {
+  
+}
+
